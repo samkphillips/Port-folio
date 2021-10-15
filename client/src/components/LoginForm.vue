e9ba953fac8a70795c4fe11e4923ddb5270a694a
Sam <sam.k.phillips@gmail.com>
2021-10-15 02:42:19 -0400

    Sketched out state management for front end auth

--- client/src/components/LoginForm.vue
+++ client/src/components/LoginForm.vue
@@ -1,8 +1,8 @@
 <template>
 <div>
-    <h1>LOGIN</h1>
+    <h1>Log In</h1>
     <form @submit.prevent="login">
-      <input v-model="username" placeholder="username" />
+      <input v-model="email" placeholder="email" type="email" />
       <input v-model="password" placeholder="password" type="password" />
       <button type="submit">Login</button>
     </form>
@@ -10,11 +10,21 @@
 </template>
 
 <script>
+import { /*SignInUser /*, RegisterUser/* */ } from '../services/Auth'
+
 export default {
   name: 'LoginForm',
   data: () => ({
-    username: '',
+    email: '',
     password: ''
-  })
+  }),
+  methods: {
+    login() {
+      // await SignInUser({  })
+      console.log(this.$root.$data.store.message)
+      this.$root.$data.store.message += '?'
+      console.log(this.$root.$data.store.message)
+    }
+  }
 }
 </script>
--- client/src/main.js
+++ client/src/main.js
@@ -6,7 +6,46 @@ import App from './App.vue'
 Vue.config.productionTip = false
 Vue.use(VueRouter)
 
+let store = {
+  message: 'Store seems to get passed down?'
+  //accessed via "this.$root.$data.store.message"
+  //auth stuff can live here I think, Vuex eat your heart out
+  //https://vuejs.org/v2/guide/state-management.html#Simple-State-Management-from-Scratch
+  /*
+  authenticated: false || localStorage.getItem('authenticated'),
+  setAuthenticated (newValue) {
+    this.authenticated = newValue
+  },
+  user: {user details, defaults null},
+  setUser (newUser) {
+    this.user = newUser
+  },
+  handleLogOut () {
+    this.user = null
+    this.authenticated = false
+    localStorage.clear()
+  },
+  async checkToken () {
+    const session = await CheckSession() //gotta add that from services
+    this.user = session
+    this.authenticated = true
+    localStorage.setItem('authenticated', '1')
+  },
+  initializeToken() {
+    const token = localStorage.getItem('token')
+    if (token) {
+      this.checkToken()
+    }
+  }
+  */
+}
+
+//store.initializeToken()
+
 new Vue({
   render: h => h(App),
-  router
+  router,
+  data: {
+    store
+  }
 }).$mount('#app')
