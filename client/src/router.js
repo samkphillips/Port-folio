e3cbe03d8867d35649033f1e9ab15c5d90a819fe
Sam <sam.k.phillips@gmail.com>
2021-10-17 02:02:43 -0400

    Protected routes up and running.

--- client/src/router.js
+++ client/src/router.js
@@ -6,13 +6,33 @@ import Portfolio from './pages/Portfolio'
 import Login from './pages/Login'
 import Account from './pages/Account'
 
-const routes = [
-  { path: '/', component: Home, name: 'Home' },
-  { path: '/about', component: About, name: 'About' },
-  { path: '/explore', component: Explore, name: 'Explore' },
-  { path: '/portfolio', component: Portfolio, name: 'Portfolio' },
-  { path: '/login', component: Login, name: 'Login' },
-  { path: '/account', component: Account, name: 'Account' }
-]
+const router = new VueRouter({
+  routes: [
+    { path: '/', component: Home, name: 'Home' },
+    { path: '/about', component: About, name: 'About' },
+    { path: '/explore', component: Explore, name: 'Explore' },
+    { path: '/portfolio', component: Portfolio, name: 'Portfolio' },
+    { path: '/login', component: Login, name: 'Login' },
+    {
+      path: '/account',
+      component: Account,
+      name: 'Account',
+      beforeEnter: async function(to, from, next) {
+        try {
+          const isAuthenticated = await router.app.$data.store.authenticated
+          if (isAuthenticated) {
+            next()
+          } else {
+            next({ path: '/login' })
+          }
+        } catch (error) {
+          console.log(error)
+          next({ path: '/login' })
+        }
+      }
+    }
+  ],
+  mode: 'history'
+})
 
-export default new VueRouter({ routes, mode: 'history' })
+export default router
