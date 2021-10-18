1eee0624a06173dc07e886a4a325f003a438c8e1
Sam <sam.k.phillips@gmail.com>
2021-10-18 07:33:34 -0400

    Styled up the home page

--- client/src/pages/Home.vue
+++ client/src/pages/Home.vue
@@ -1,7 +1,9 @@
 <template>
-  <div>
-    <h2>Home</h2>
-    <img src="https://source.unsplash.com/random" />
+  <div class="home-body">
+    <h2>Welcome to Port-Folio</h2>
+    <p>Port-Folio is the world's least popular online portfolio service. It is free, though, so that's cool.</p>
+    <p>Sign up for free today and get your art online for the world to see.</p>
+    <img src="https://source.unsplash.com/random/?art" class="fit" />
   </div>
 </template>
 
@@ -10,3 +12,18 @@ export default {
   name: 'Home'
 }
 </script>
+
+<style scoped>
+.home-body {
+  width: calc(100vw - 15px);
+  padding: 0 0 70px 0;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+
+.fit {
+  max-width: 90%;
+}
+</style>
