ac15c81ae7164c6233dc757fbb0814a3fa76b8ce
Sam <sam.k.phillips@gmail.com>
2021-10-18 03:56:55 -0400

    Blocking out account settings page

--- client/src/pages/Account.vue
+++ client/src/pages/Account.vue
@@ -1,12 +1,38 @@
 <template>
   <div>
     <h2>Account</h2>
-    <p>Settings and etc.</p>
+    <div v-if="userData">
+      <p>Settings and etc.</p>
+      <p>Hello, {{ userData.username }}</p>
+    </div>
+    <div v-else>
+      <h5>Gathering your account data, this may take a moment.</h5>
+    </div>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
+
+import { BASE_URL } from '../globals'
+
 export default {
-  name: 'Account'
+  name: 'Account',
+  data: () => ({
+    userData: null
+  }),
+  computed: {
+    user: function () {return this.$root.$data.store.user}
+  },
+  mounted() {
+    this.fetchUserData()
+  },
+  methods: {
+    async fetchUserData() {
+      const res = await axios.get(`${BASE_URL}/user/byId/${this.user.id}`)
+      console.log(res)
+      this.userData = res.data
+    }
+  }
 }
 </script>
