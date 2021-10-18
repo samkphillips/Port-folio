474d6ebcee6774578839b64a4583e06fd656d2c2
Sam <sam.k.phillips@gmail.com>
2021-10-18 07:07:05 -0400

    Styled up nav bar

--- client/src/components/Nav.vue
+++ client/src/components/Nav.vue
@@ -1,16 +1,23 @@
 <template>
   <nav>
-    <router-link class="link" to="/">Home</router-link>
-    <router-link class="link" to="/about">About</router-link>
-    <router-link class="link" to="/explore">Explore</router-link>
-    <router-link class="link" to="/portfolio/1">Portfolio[Temp Link to port 1]</router-link>
-    <router-link class="link" to="/login">Login/Signup</router-link>
-    <router-link class="link" to="/account">Account</router-link>
-    <div v-if="authenticated && user">
-      <h5>Hello, {{ user.email }}!</h5>
-      <button @click="logOut">Log Out</button>
+    <div>
+      <h2>Port-Folio</h2>
     </div>
-    <h5 v-else>User not signed in.</h5>
+    <div>
+      <router-link class="link" to="/">Home</router-link>
+      <router-link class="link" to="/explore">Explore</router-link>
+      <router-link class="link" to="/about">About</router-link>
+    </div>
+    <div>
+      <div v-if="authenticated && user" class="flex-row">
+        <h5>Hello, {{ user.email }}!</h5>
+        <router-link class="link" to="/account">Account</router-link>
+        <button @click="logOut">Log Out</button>
+      </div>
+      <div v-else>
+        <router-link class="link" to="/login">Login/Signup</router-link>
+      </div>
+    </div>
   </nav>
 </template>
 
@@ -30,11 +37,47 @@ export default {
 </script>
 
 <style scoped>
-nav {
-  background-color: antiquewhite;
+* {
+  color: #F0F1F9;
 }
 
+nav {
+  background-color: #07189D;
+  height: 70px;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.flex-row {
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+}
+
+h2,
 .link {
   margin: 0 10px;
+}
+
+h5 {
+  margin: 0 20px;
+}
+
+button {
+  background-color: #F0F1F9;
+  color: #7582E6;
+  border: none;
+  border-radius: 20px;
+  padding: 5px;
+  margin: 0 10px;
+}
+
+button:hover {
+  cursor: pointer;
+  background-color: #7582E6;
+  color: #F0F1F9;
 }
 </style>
